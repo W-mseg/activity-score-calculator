631685adce0643582260a0304144a2aa64eec410
W-mseg <marco.segretario2705@gmail.com>
2021-09-14 11:29:07 +0200

    cta

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 
+document.getElementById('posts').addEventListener('change', () => {
+    if (typeof +document.getElementById('posts').value === typeof 1){
+        document.getElementById('posts').classList.add('valid')
+        document.getElementById('posts').classList.remove('error')
+    }
+    else{
+        document.getElementById('posts').classList.remove('valid')
+        document.getElementById('posts').classList.add('error')
+    }
+});
+document.getElementById('comments').value;
+document.getElementById('reactions').value;
+document.getElementById('activemembers').value;
+
 document.getElementById("buttoncheck").addEventListener(('click'),() => {
     let posts = document.getElementById('posts').value;
     let comments = document.getElementById('comments').value;
